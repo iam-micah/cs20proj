b3f1acb6cdac3fde7eae224bda703580ff490f48
Walter254 <53183305+Walter254@users.noreply.github.com>
2022-12-08 22:31:11 -0500

    Add files via upload

--- populate_db.php
+++ populate_db.php
@@ -1,54 +1,43 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
+<?php 
 
-<body>
-    <script>
-console.log("heyyy process page");
-
-</script>
-	<?php
-
-    // getting all the variables
-    $first_name = $_GET['Fname'];
-    $last_name = $_GET['Lname'];
-    $email = $_GET['Email'];
-    $house_number = $_GET['House_number'];
-    $street = $_GET['Street'];
-    $city = $_GET['City'];
-    $zip = $_GET['Zip'];
-    $price = $_GET['Price'];
-    $bathrooms = $_GET['Bathrooms'];
-    $description = $_GET['Descriptions'];
-    $start_date = $_GET['Date_Start_Test_DatetimeLocal'];
-    $end_date = $_GET['Date_End_Test_DatetimeLocal'];
-    $room_image = $_GET['room_photo'];
-
-    $location = $house_number." ".$street." ".$City." ".$zip;
-
-    //establish connection with database
-    $server = "localhost";
+//establish connection with database
+    $server = "127.0.0.1";
     $userid = "ue25bvzryotbr";
     $pw = "klcyqkgf6pir";
     $db= "db4m4v99nim28z";
 
     //create connections
-    $conn = new mysql($server, $userid, $pw);
+    $conn = new mysqli($server, $userid, $pw);
 
     //check connection
     if($conn->connect_error){
         die("connection failed: ". $conn->connect_error);
     }
+    // getting all the variables
+    $first_name = $_POST['Fname'];
+    $last_name = $_POST['Lname'];
+    $email = $_POST['Email'];
+    $house_number = $_POST['House_number'];
+    $street = $_POST['Street'];
+    $city = $_POST['City'];
+    $zip = $_POST['Zip'];
+    $price = $_POST['Price'];
+    $bathrooms = $_POST['Bathrooms'];
+    $description = $_POST['Descriptions'];
+    $start_date = $_POST['Date_Start_Test_DatetimeLocal'];
+    $end_date = $_POST['Date_End_Test_DatetimeLocal'];
+    $room_image = $_POST['room_photo'];
+
+    $location = $house_number." ".$street." ".$City." ".$zip;
+    echo "console.log('help');";
+   
 
 
     //run a query
-    $sql = "INSERT INTO `RoomData`(`fname`, `lname`, `location`, `email`, `price`, `num_bathrooms`, `description`, `image`, `move_in_date`, `move_out_date`)
-    VALUES (".$first_name.",".$last_name .",".$location.",".$email",".$price.",".$bathrooms.",".$description.",".$room_image.",".$start_date.",".$end_date.")";
+    $sql = "INSERT INTO RoomData(`fname`, `lname`, `location`, `email`, `price`, `num_bathrooms`, `description`, `image`, `move_in_date`, `move_out_date`)
+        VALUES (".$first_name.",".$last_name.",".$location.",".$email.",".$price.",".$bathrooms.",".$description.",".$room_image.",".$start_date.",".$end_date.")";
 
-    $conn->query($sql);
+    $run = $conn->query($sql);
 
 
 
@@ -56,6 +45,4 @@ console.log("heyyy process page");
        
  //close connection
  $conn->close();
-    ?>
-</body>
-</html>
+?>
