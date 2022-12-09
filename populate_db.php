49d5207655ba8de0ec8c60cb60b9963a85432626
Walter254 <53183305+Walter254@users.noreply.github.com>
2022-12-09 17:18:27 -0500

    Add files via upload

--- populate_db.php
+++ populate_db.php
@@ -14,8 +14,8 @@
         die("connection failed: ". $conn->connect_error);
     }
 
- 
-   $conn->select_db($db);
+    //select the database
+    $conn->select_db($db);
 
     // getting all the variables
     $first_name = $_POST['Fname'];
@@ -27,22 +27,23 @@
     $zip = $_POST['Zip'];
     $price = $_POST['Price'];
     $bathrooms = $_POST['Bathrooms'];
-    $description = $_POST['Descriptions'];
-    $start_date = $_POST['Date_Start_Test_DatetimeLocal'];
-    $end_date = $_POST['Date_End_Test_DatetimeLocal'];
+    $description = $_POST['Description'];
+    $availability = $_POST['availability'];
     $room_image = $_POST['room_photo'];
 
-    $Location = $house_number." ".$street." ".$City." ".$zip;
+    $Location = $house_number." ".$street." ".$city." ".$zip;
     
+   
+
 
     //run a query
-    $query = "INSERT INTO RoomData (fname,lname,location,email,price,num_bathrooms,description,image,move_in_date,move_out_date)
-        VALUES ('$first_name','$last_name','$Location','$email','$price','$bathrooms','$description','$room_image','$start_date','$end_date')";
+    $query = "INSERT INTO RoomData (fname,lname,location,email,price,num_bathrooms,description,image,availability)
+        VALUES ('$first_name','$last_name','$Location','$email','$price','$bathrooms','$description','$room_image','$availability')";
 
-     $run = $conn->query($query);
+    $run = $conn->query($query);
 
     if($run){
-        echo"submitted successfully";
+        echo"submitted successfully. If you received Warnings about your address, you might want to verify that it is right then resubmit the form!";
     }
     else{
         echo"not submitted";
