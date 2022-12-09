cad9ed72aa85aca5dec0e8ba3d1ad37a9fe6301f
Walter254 <53183305+Walter254@users.noreply.github.com>
2022-12-09 11:56:50 -0500

    Add files via upload

--- populate_db.php
+++ populate_db.php
@@ -1,5 +1,7 @@
 <?php 
 
+header('Access-Control-Allow-Origin: *');
+
 //establish connection with database
     $server = "127.0.0.1";
     $userid = "ue25bvzryotbr";
@@ -13,6 +15,10 @@
     if($conn->connect_error){
         die("connection failed: ". $conn->connect_error);
     }
+
+ 
+   $conn->select_db($db);
+
     // getting all the variables
     $first_name = $_POST['Fname'];
     $last_name = $_POST['Lname'];
@@ -28,21 +34,24 @@
     $end_date = $_POST['Date_End_Test_DatetimeLocal'];
     $room_image = $_POST['room_photo'];
 
-    $location = $house_number." ".$street." ".$City." ".$zip;
-    echo "console.log('help');";
-   
-
+    $Location = $house_number." ".$street." ".$City." ".$zip;
+    
 
     //run a query
-    $sql = "INSERT INTO RoomData(`fname`, `lname`, `location`, `email`, `price`, `num_bathrooms`, `description`, `image`, `move_in_date`, `move_out_date`)
-        VALUES (".$first_name.",".$last_name.",".$location.",".$email.",".$price.",".$bathrooms.",".$description.",".$room_image.",".$start_date.",".$end_date.")";
+    $query = "INSERT INTO RoomData (fname,lname,location,email,price,num_bathrooms,description,image,move_in_date,move_out_date)
+        VALUES ('$first_name','$last_name','$Location','$email','$price','$bathrooms','$description','$room_image','$start_date','$end_date')";
 
-    $run = $conn->query($sql);
+     $run = $conn->query($query);
 
+    if($run){
+        echo"submitted successfully";
+    }
+    else{
+        echo"not submitted";
+    }
 
 
     
-       
  //close connection
  $conn->close();
 ?>
